79a80caf9fc8bcb91cae481cb57795daeb46fbfa
aqiqi123 <huangqisi7624@qq.com>
2025-12-08 22:11:21 +0800

    feat(date):add lots of CharacterSO,WeaponSO,PassiveItemSO,EnemySO

--- Assets/Scripts/ScriptableObject/CharacterSO.cs
+++ Assets/Scripts/ScriptableObject/CharacterSO.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="CharacterSO",menuName ="SOs/Character")]
 public class CharacterSO : ScriptableObject
 {
+    [SerializeField] private string characterName;
+    [SerializeField] private Sprite icon;
+    [Space]
     [SerializeField] private float maxHealth;
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float might;
-    [SerializeField] private float cooldownReduction;
+    [Tooltip("例如 2 代表武器有双倍伤害")][SerializeField] private float might;
+    [Tooltip("例如 0.05 代表减少 5% 冷却")][SerializeField] private float cooldownReduction;
     [SerializeField] private int additionalProjectileCount;
-    [SerializeField] private float projectileSpeed;
+    [Tooltip("例如 1.1 代表增加 10% 子弹飞行速度")][SerializeField] private float projectileSpeed;
     [SerializeField] private float durationMultiplier;
     [SerializeField] private float areaMultiplier;
 
+    public string CharacterName=>characterName;
+    public Sprite Icon=>icon;
     public float MaxHealth=>maxHealth;
     public float MoveSpeed=> moveSpeed;
     public float Might => might;
--- Assets/Scripts/ScriptableObject/EnemySO.cs
+++ Assets/Scripts/ScriptableObject/EnemySO.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemySO", menuName = "SOs/Enemy")]
 public class EnemySO : ScriptableObject
 {
+    [SerializeField] private string enemyName;
+    [SerializeField] private Sprite icon;
+    [Space]
     [SerializeField] private float maxHealth;
     [SerializeField] private float moveSpeed;
     [SerializeField] private float damage;
 
+    public string EnemyName=>enemyName;
+    public Sprite Icon=>icon;
     public float MaxHealth=>maxHealth;
     public float MoveSpeed=>moveSpeed;
     public float Damage=>damage;
