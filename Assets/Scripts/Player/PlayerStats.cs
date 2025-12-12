d9a6b240aa597c827715ba4c3b1f7c543a10b20e
aqiqi123 <huangqisi7624@qq.com>
2025-12-12 20:22:03 +0800

    fix:build bug

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 [RequireComponent(typeof(HealthSystem))]
 public class PlayerStats : MonoBehaviour
